005aded4de45ff56e1b1eed9be3070586f0db9a8
Arthur <apploidyakutsk@gmail.com>
2018-10-23 23:37:44 +0900

    runnable

---
+++ Main.py
@@ -0,0 +1,35 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+# --
+from vk_bot import VkBot
+# --
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+
+# API-ключ созданный ранее
+token = "6a9c267cd469388709a9e9acaddbe0aa81a0abbf12239b3e597a31729ffbddb9c88e80a443554c918b8f7"
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+print("Server started")
+for event in longpoll.listen():
+
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        if event.to_me:
+
+            print('New message:')
+            print(f'For me by: {event.user_id}', end='')
+
+            bot = VkBot(event.user_id)
+            write_msg(event.user_id, bot.new_message(event.text))
+
+            print('Text: ', event.text)
