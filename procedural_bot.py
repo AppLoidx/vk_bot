7e982f5d2345c8d25ae6b64f5b0b3e0a85766b4a
Arthur <apploidyakutsk@gmail.com>
2018-10-23 22:41:18 +0900

    Initial commit

---
+++ procedural_bot.py
@@ -0,0 +1,38 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+
+# API-ключ созданный ранее
+token = "6a9c267cd469388709a9e9acaddbe0aa81a0abbf12239b3e597a31729ffbddb9c88e80a443554c918b8f7"
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+print("Бот запущен")
+# Основной цикл
+for event in longpoll.listen():
+
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+
+            # Сообщение от пользователя
+            request = event.text
+
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
+
